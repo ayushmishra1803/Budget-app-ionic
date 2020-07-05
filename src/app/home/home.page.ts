e1eb78ff565e306aa113690cc3c66195239844c6
Ayush-Mishra <ayushmishra0810@gmail.com>
2020-07-06 00:38:42 +0530

    Updates

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"],
 })
 export class HomePage {
-
+  enteredexpences:string;
+  enteredammount:number
+ 
   constructor() {}
-
+  clear(){
+    console.log(`${this.enteredexpences}${this.enteredammount}`);
+    
+  };
 }
