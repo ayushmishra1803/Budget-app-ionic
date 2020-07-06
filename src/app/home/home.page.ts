6aeef0ff5750d3739469f2440d0b5163356c0a88
Ayush-Mishra <ayushmishra0810@gmail.com>
2020-07-06 18:08:43 +0530

    Updates

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,16 +1,19 @@
 import { Component } from "@angular/core";
 
+import { AlertController } from '@ionic/angular';
 @Component({
   selector: "app-home",
   templateUrl: "home.page.html",
   styleUrls: ["home.page.scss"],
 })
 export class HomePage {
+  constructor(public alertController: AlertController) {}
   enteredexpences: string;
   enteredammount: number;
-expencesreason:string[]=[];
-expencesammount:number[]=[];
-  constructor() {}
+  expencesreason: string[] = [];
+  expencesammount: number[] = [];
+  
+  
   clear() {
     if (this.enteredammount > 0 && this.enteredexpences.length > 0) {
       this.enteredexpences = "";
@@ -19,12 +22,9 @@ expencesammount:number[]=[];
   }
   add() {
     if (this.enteredammount > 0 && this.enteredexpences.length > 0) {
-        
       this.expencesreason.push(this.enteredexpences);
       this.expencesammount.push(this.enteredammount);
-        
-     
-      
+    }
+   
   }
-}
 }
